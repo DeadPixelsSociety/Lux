3a2e212575398fe5446adb8b85313e0173bd526c
ahugeat <hugeat.arthur@gmail.com>
2015-10-05 15:39:21 +0200

    Reduce the logo.

--- src/local/Logos.cc
+++ src/local/Logos.cc
@@ -39,31 +39,38 @@ Logos::Logos(game::ResourceManager &resources, game::WindowGeometry &geometry)
   assert(m_ufcTexture != nullptr);
 }
 
-static constexpr float LOGO_WIDTH = 128.0f;
+
+static constexpr float LOGO_WIDTH = 80.0f;
+static constexpr float LOGO_RATIO = LOGO_WIDTH / 128.0f;
+
 static constexpr float RIGHT_PADDING = 15.0f;
-static constexpr float BOTTOM_PADDING = 76.0f;
-static constexpr float SECOND_LINE_PADDING = BOTTOM_PADDING + 15.0f + 98.0f;
-static constexpr float FCEC_PADDING = (36 - 61) / 2.0f;
-static constexpr float UFR_PADDING = (73 - 98) / 2.0f;
+static constexpr float BOTTOM_PADDING = 15.0f + 61.0f * LOGO_RATIO;
+static constexpr float SECOND_LINE_PADDING = BOTTOM_PADDING + 15.0f + 98.0f * LOGO_RATIO;
+static constexpr float FCEC_PADDING = (36 * LOGO_RATIO - 61 * LOGO_RATIO) / 2.0f;
+static constexpr float UFR_PADDING = (73 * LOGO_RATIO - 98 * LOGO_RATIO) / 2.0f;
 
 void Logos::render(sf::RenderWindow& window) {
   sf::Sprite spriteDPS;
   spriteDPS.setTexture(*m_dpsTexture);
+  spriteDPS.setScale(LOGO_RATIO, LOGO_RATIO);
   spriteDPS.setPosition(RIGHT_PADDING, m_geometry.getYFromBottom(BOTTOM_PADDING));
   window.draw(spriteDPS);
 
   sf::Sprite spriteFCEC;
   spriteFCEC.setTexture(*m_fcecTexture);
+  spriteFCEC.setScale(LOGO_RATIO, LOGO_RATIO);
   spriteFCEC.setPosition(RIGHT_PADDING * 2.0f + LOGO_WIDTH, m_geometry.getYFromBottom(BOTTOM_PADDING + FCEC_PADDING));
   window.draw(spriteFCEC);
 
   sf::Sprite spriteUFR;
   spriteUFR.setTexture(*m_ufrTexture);
+  spriteUFR.setScale(LOGO_RATIO, LOGO_RATIO);
   spriteUFR.setPosition(RIGHT_PADDING, m_geometry.getYFromBottom(SECOND_LINE_PADDING + UFR_PADDING));
   window.draw(spriteUFR);
 
   sf::Sprite spriteUFC;
   spriteUFC.setTexture(*m_ufcTexture);
+  spriteUFC.setScale(LOGO_RATIO, LOGO_RATIO);
   spriteUFC.setPosition(RIGHT_PADDING * 2.0f + LOGO_WIDTH, m_geometry.getYFromBottom(SECOND_LINE_PADDING));
   window.draw(spriteUFC);
 }
